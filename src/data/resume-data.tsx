000706c510c92294ac2c5f0934a3810b5552a861
Sayan Mukherjee <157164979+SayanmukherG@users.noreply.github.com>
2024-01-20 03:10:03 +0530

    Update resume-data.tsx

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -22,10 +22,10 @@ import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
 export const RESUME_DATA = {
   name: "Sayan Mukherjee",
   initials: "SM",
-  location: "Room 609, Economic Research Unit, Indian Statistical Institute, Kolkata",
+  location: "Economic Research Unit, Indian Statistical Institute, Kolkata",
   locationLink: "",
   about:
-    "Welcome to my homepage! I am currently a final year graduate student",
+    "Welcome to my homepage! I am currently a final year graduate student working on evolution and learning in games under the supervision of Prof. Souvik Roy.",
   summary:
     "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience in working remotely with companies all around the world.",
   avatarUrl: "https://avatars.githubusercontent.com/u/157164979?s=400&u=1dbbca65c2e8fffdbe89f2922404439042c7b4ed&v=4",
